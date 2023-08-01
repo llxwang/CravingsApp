35c406dd13ba5eb55154b427cdb81dfad7922226
Lily Wang <lilyhdaa@gmail.com>
2023-08-01 13:56:58 -0700

    Initial Commit

---
+++ CravingsApp/CravingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CravingsAppApp.swift
+//  CravingsApp
+//
+//  Created by JZhang on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CravingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
